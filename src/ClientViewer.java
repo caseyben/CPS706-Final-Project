0604b4cd23bb42b68ee630ccb26ad9551cc28594
Casey Smith <casey.smith13@yahoo.ca>
2019-03-05 17:18:48 -0500

    Update ClientViewer.java

--- src/ClientViewer.java
+++ src/ClientViewer.java
@@ -1,14 +1,11 @@
 import javax.swing.JFrame;
 
-public class ClientViewer
-{  
-	
-   public static void main(String[] args)
-   {  
-      JFrame frame = new ClientFrame();
-      frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-      frame.setTitle("P2P Client");
-      frame.setVisible(true);      
-   }
+public class ClientViewer{
 
+    public static void main(String[] args){
+        JFrame frame = new ClientFrame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setTitle("P2P Client");
+        frame.setVisible(true);
+    }
 }
