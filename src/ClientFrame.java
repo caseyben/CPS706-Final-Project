f90155e2190b2d9f67e4c05390aef66b2cfc3cec
Casey Smith <casey.smith13@yahoo.ca>
2019-03-05 17:18:21 -0500

    Update ClientFrame.java

--- src/ClientFrame.java
+++ src/ClientFrame.java
@@ -1,9 +1,8 @@
 import javax.swing.JFrame;
 
 public class ClientFrame extends JFrame{
-	public ClientFrame()
-	{
-		setSize(800,600);
-		setResizable(false);
-	}
+    public ClientFrame(){
+        setSize(800,600);
+        setResizable(false);
+    }
 }
