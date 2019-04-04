c5e30f6fbad6bd9a812531530645947238f34ab6
brian-lacdoo <brian.lacdoo@hotmail.com>
2019-04-04 00:45:56 -0400

    Update P2P_Server.java

--- src/P2P_Server.java
+++ src/P2P_Server.java
@@ -3,10 +3,10 @@ import java.io.*;
 import java.util.*;
 
 public class P2P_Server{
-    final int OK = 200;
-	final int BAD_REQUEST = 400;
-	final int NOT_FOUND = 404;
-	final int HTTP_NOT_SUPPORTED = 505;
+    final private int OK = 200;
+	final private int BAD_REQUEST = 400;
+	final private int NOT_FOUND = 404;
+	final private int HTTP_NOT_SUPPORTED = 505;
 
 	private int port = 20440;
 	private ServerSocket socket;
@@ -32,7 +32,8 @@ public class P2P_Server{
 
 				String response = in.readUTF();
 
-			
+				response = OK + " " + port;
+				connectionSocket.close();		
 			}//while
 		}//run
 	};
