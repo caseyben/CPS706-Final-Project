31d54d76d8ab4638c366d2636106e13432b54a6b
Andy Ho <hoandy1998@gmail.com>
2019-04-04 03:44:35 -0400

    DHT

--- src/DHT.java
+++ src/DHT.java
@@ -1,89 +1,124 @@
 import java.net.*;
 import java.io.*;
+import java.util.*;
 
 public class DHT {
     private static final int NUM_DHT_SERVERS = 4;
+    private static Hashtable<String, String> theTable = new Hashtable<>();
+
+    public static int DHT_ID = 0;
+    public static String DHT_IP_Address = "";
+    public static int DHT_PORT = 0;
+    public static String SUCCESSOR_IP = "";
+
     public static void main(String[] args) throws IOException {
         //TODO Hashtable(K, V) = (Name of file wanted, IP address of client with it)
 
+        //init();
         String key = "kimi no na wa";
         //System.out.println(hashKey(key));
 
-        UDPServer();
+        UDPServer(25565);
 
         //TCPServer(25565);
         //TCPServer(25566);
+        //TCPServer(25567);
+        //TCPServer(25568);
+
         //TCPClient(25565);
         //TCPClient(25566);
+        //TCPClient(25567);
+        //TCPClient(25568);
     }
 
-    public static void UDPServer() throws IOException { //UDP from Client --> Server
-        DatagramSocket DHTSocket = new DatagramSocket(25565);
+    public static void UDPClient(String message, String IP, int port) throws IOException{
+        DatagramSocket UDP_ClientSocket = new DatagramSocket(25565);
+        //Prepare Message
+        byte[] UDP_sendData = new byte[4096];
+        UDP_sendData = message.getBytes();
 
-        byte[] receiveDataBuffer = new byte[4096];
-        byte[] returnDataBuffer = new byte[4096];
+        InetAddress UDP_returnIPAddress_ = InetAddress.getByName(IP);
+
+        DatagramPacket sendPacket = new DatagramPacket(UDP_sendData, UDP_sendData.length, UDP_returnIPAddress_, port);
+
+        UDP_ClientSocket.send(sendPacket);
+        UDP_ClientSocket.close();
+        UDPServer(25565);
+    }
+
+    public static void UDPServer(int port) throws IOException { //UDP from Client --> Server
+        DatagramSocket UDP_ServerSocket = new DatagramSocket(25565);
+        System.out.println("UDP_Server on port: " + port);
+
+        //Data Buffers
+        byte[] UDP_receiveDataBuffer = new byte[4096];
+        byte[] UDP_returnDataBuffer = new byte[4096];
 
         while(true){
-            DatagramPacket receivePacket = new DatagramPacket(receiveDataBuffer, receiveDataBuffer.length);
+            //In
+            DatagramPacket UDP_receivePacket = new DatagramPacket(UDP_receiveDataBuffer, UDP_receiveDataBuffer.length);
+            UDP_ServerSocket.receive(UDP_receivePacket);
+            String UDP_receiveString = new String(UDP_receivePacket.getData(), 0, UDP_receivePacket.getLength());
+            System.out.println(UDP_receiveString);
 
-            DHTSocket.receive(receivePacket);
+            //Out
+            InetAddress UDP_returnAddress = UDP_receivePacket.getAddress();
+            int UDP_returnPort = UDP_receivePacket.getPort();
 
-            String string = new String(receivePacket.getData());
+            String UDP_receiveArray[] = UDP_receiveString.split("~");
 
-            System.out.println(string);
 
-            InetAddress returnAddress = receivePacket.getAddress();
-            int returnPort = receivePacket.getPort();
-            returnDataBuffer = string.getBytes(); //(string + "Acknowledged from" + returnAddress).getBytes();
 
-            DatagramPacket returnPacket = new DatagramPacket(returnDataBuffer, returnDataBuffer.length, returnAddress, returnPort);
-            DHTSocket.send(returnPacket);
 
+            if(UDP_receiveArray[0].equals("FIND")){
+                System.out.println("find");
+                UDP_ServerSocket.close();
+                retreive(UDP_receiveArray[1], UDP_returnAddress.getHostAddress(), UDP_returnPort);
+                break;
+            } else if (UDP_receiveArray[0].equals("INSERT")){
+                System.out.println("insert");
+                insert(UDP_receiveArray[1], UDP_returnAddress.getHostAddress());
+            }
+
+            //String UDP_returnString = "192.168.2.1:1111_192.168.2.2:2222_192.168.2.3:3333"; //string + " acknowledged from: " + returnAddress;
+            //UDP_returnDataBuffer = UDP_returnString.getBytes();
+
+            //DatagramPacket UDP_returnPacket = new DatagramPacket(UDP_returnDataBuffer, UDP_returnDataBuffer.length, UDP_returnAddress, UDP_returnPort);
+            //UDP_ServerSocket.send(UDP_returnPacket);
         }
     }
 
-
-
     public static void TCPClient(int port) throws IOException {
         //Establish Socket
-        Socket clientSocket = new Socket("127.0.0.1", port);
-        System.out.println("Client on port: " + port);
-
-        //In
-        BufferedReader fromTCPServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-        String fromServerText = fromTCPServer.readLine();
-        System.out.println("Received from Server: " + fromServerText);
-
+        Socket TCP_clientSocket = new Socket("127.0.0.1", port);
+        System.out.println("TCP_Client on port: " + port);
 
         //Out
-        DataOutputStream toServer = new DataOutputStream(clientSocket.getOutputStream());
+        DataOutputStream TCP_toServer = new DataOutputStream(TCP_clientSocket.getOutputStream());
+        TCP_toServer.writeBytes("This is TCP_client on port: " + port + " " + '\n');
 
-        toServer.writeBytes("Returning on port" + port + '\n');
+        //In
+        BufferedReader TCP_fromTCPServer = new BufferedReader(new InputStreamReader(TCP_clientSocket.getInputStream()));
+        String TCP_fromServerText = TCP_fromTCPServer.readLine();
+        System.out.println("Received from TCP_Server: " + TCP_fromServerText);
     }
 
     public static void TCPServer(int port) throws IOException {
         //Establish Socket
-        ServerSocket serverSocket = new ServerSocket(port);
-        System.out.println("Server on port: " + port);
+        ServerSocket TCP_serverSocket = new ServerSocket(port);
+        System.out.println("TCP_Server on port: " + port);
 
          while(true){
              //Establish Socket
-             Socket serverConnectionSocket = serverSocket.accept();
-
-             //Data streams
-             BufferedReader fromClient = new BufferedReader(new InputStreamReader(serverConnectionSocket.getInputStream()));
-             DataOutputStream toClient = new DataOutputStream(serverConnectionSocket.getOutputStream());
+             Socket TCP_serverConnectionSocket = TCP_serverSocket.accept();
 
              //In
-             String fromClientText = fromClient.readLine();
-             System.out.println(fromClientText);
+             BufferedReader TCP_fromClient = new BufferedReader(new InputStreamReader(TCP_serverConnectionSocket.getInputStream()));
+             String TCP_fromClientText = TCP_fromClient.readLine();
 
              //Out
-             toClient.writeBytes(fromClientText + "ACK" + '\n');
-
-
-
-
+             DataOutputStream TCP_toClient = new DataOutputStream(TCP_serverConnectionSocket.getOutputStream());
+             TCP_toClient.writeBytes(TCP_fromClientText + " ACK!" + '\n');
          }
     }
 
@@ -96,12 +131,61 @@ public class DHT {
         return (ASCIISum % NUM_DHT_SERVERS) + 1;
     }
 
-    //TODO Initialise IP and port() (?)
+    public static void delimit(String UDP_receiveString){
 
-    //TODO insert(K, V)
+    }
+
+    public static void insert(String content, String IPAddress){ //P2P_Client inform_and_update()
+        theTable.put(content, IPAddress);
+    }
 
     //TODO retreive(hash(K))
         //TODO returnIP()
+    public static void retreive(String content, String IPAddress, int port) throws IOException{ //P2P_Client query_for_content()
+        if(theTable.containsKey(content)){
+            UDPClient(theTable.get(content), IPAddress, port);
+        } else {
+            UDPClient("404 content not found", IPAddress , port);
+        }
+    }
 
     //TODO remove(K, V)
+    public static void remove(String content, String IPAddress){ //P2P_Client exit()
+        if(theTable.containsKey(content)){
+            theTable.remove(content);
+        } else {
+            forwardEntry(content, IPAddress);
+        }
+    }
+
+    //TODO return_DHT_IPs
+
+    //TODO Error checking
+    public static void init(){
+        Scanner initScanner = new Scanner(System.in);
+
+        System.out.println("Please enter DHT_ID between 1-4: ");
+        DHT_ID = initScanner.nextInt();
+
+        System.out.println("Please enter DHT_IP_Address: ");
+        DHT_IP_Address = initScanner.next().trim();
+
+        System.out.println("Please enter DHT_PORT: ");
+        DHT_PORT = initScanner.nextInt();
+
+        System.out.println("Please enter SUCCESSOR_IP: ");
+        SUCCESSOR_IP = initScanner.next().trim();
+
+        initScanner.close();
+
+        System.out.println("DHT_ID: " + DHT_ID);
+        System.out.println("DHT_IP_Address: " + DHT_IP_Address);
+        System.out.println("DHT_PORT: " + DHT_PORT);
+        System.out.println("SUCCESSOR_IP: " + SUCCESSOR_IP);
+    }
+
+    //TODO forward_table_entry
+    public static void forwardEntry(String content, String IPAddress){
+        //call TCP Client to send data
+    }
 }
